f82405d1d87076043b8d43249ce9386c954cb192
Kushagra Pathak <201801005@daiict.ac.in>
2024-02-18 18:39:53 +0530

    [feat]: some css related changes

--- app/controllers/SubscriptionController.js
+++ app/controllers/SubscriptionController.js
@@ -1,6 +1,7 @@
 import PushNotificationSender from '../classes/PushNotificationSender.js'
 import MailNotificationSender from '../classes/MailNotificationSender.js'
 import _get from 'lodash/get.js'
+import logger from '../utils/logger.js'
 
 function configureSendService(request) {
 	const isMail = _get(request, 'query.mail')
@@ -9,6 +10,11 @@ function configureSendService(request) {
 export default async function SubscriptionController(request, response) {
 	const Service = configureSendService(request)
 	const sender = new Service()
-	await sender._send(request)
+	try {
+		await sender._send(request)
+	} catch (err) {
+		logger.log('error', err.toString())
+		response.send('Service not available').status(500)
+	}
 	response.send('Notification sent!').status(200)
 }
